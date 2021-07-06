7b4d121f7fdaf933d31dd54d9d6720b7b7f9f249
Rohan7546 <58820449+Rohan7546@users.noreply.github.com>
2021-07-06 14:28:59 +0530

    Update Finding_prime_1to_n.cpp

--- Number_Theory_1/Finding_prime_1to_n.cpp
+++ Number_Theory_1/Finding_prime_1to_n.cpp
@@ -1,1 +1,39 @@
+ #include<bits/stdc++.h>
+using namespace std;
+
+int isprime(int n){
+   
+    bool prime[n+1];
+    for(int i=0;i<n+1;i++){
+        prime[i]=true;
+    }
+    prime[0]=false;
+    prime[1]=false;
+    for(int i=2;i*i<=n;i++){
+        
+        if(prime[i]){
+            for(int j=i*i;j<=n;j+=i){
+                prime[j]=false;
+            }
+        }
+    }
+    
+    
+    int cnt=0;
+    for(int j=0;j<=n;j++){
+        if(prime[j])cnt++;
+    }
+    return cnt;
+}
+
+
+int main(){
+    
+    // write your code here
+    int n;
+    cin>>n;
+    
  
+    cout<<isprime(n)<<endl;
+    return 0;
+}
